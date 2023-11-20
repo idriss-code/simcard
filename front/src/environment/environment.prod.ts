6d30067ac5c72c4206acc60af2b423c604b4575f
benlamouche <benlamouche78@hotmail.fr>
2023-11-20 14:02:12 +0100

    fix api url

--- front/src/environment/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    api: 'http://debian-noir:8080/api/simcards'
-  };
-  
---
+++ front/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    api: 'http://benhamadouche.ddns.net:8080/api/simcards'
+  };
+  
